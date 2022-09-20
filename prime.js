55332dfa242d4a50682e8e43bc94a6fbb175c1ad
Ojas <ojasbendale18200@gmail.com>
2022-09-20 11:33:19 +0530

    codeix_by_ojas

---
+++ prime.js
@@ -0,0 +1,20 @@
+function prime(num){
+    let count = 0
+    for(let i =1;i<=num;i++){
+        if(num%i == 0){
+           count++;
+        }
+
+        if(count ==2){
+            return true;
+        }
+    }
+    return false
+}
+
+let ans = prime(13)
+if(ans == true){
+    console.log("Prime")
+} else {
+    console.log("Not Prime")
+}
